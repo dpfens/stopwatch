01ee8ec3f50b15fe98bae0ccdc7b500efd905180
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2020-05-20 22:31:48 -0400

    resolves #1 and #2

--- test.js
+++ test.js
@@ -5,12 +5,12 @@ instance.start();
 
 setTimeout(function() {
   var split = instance.takeSplit();
-  console.log(instance.splits);
 }, 25);
 
 setTimeout(function() {
   var split = instance.takeSplit();
   console.log(instance.splits);
+  //console.log('testing addSplit');
   instance.addSplit(10);
   console.log(instance.splits);
   instance.addSplit(40);
@@ -18,3 +18,31 @@ setTimeout(function() {
   instance.addSplit(1000);
   console.log(instance.splits);
 }, 1000);
+
+setTimeout(function() {
+    console.log('testing removeSplit');
+    console.log(instance.splits);
+    instance.removeSplit(-1);
+    var totalDuration = instance.totalDuration();
+    console.log(instance.splits);
+    console.log('totalDuration: ' + totalDuration);
+    instance.addSplit(totalDuration);
+    console.log(instance.splits);
+    console.log(instance.totalDuration());
+    console.log(instance.splitDuration());
+    instance.stop();
+}, 2000);
+
+
+setTimeout(function() {
+    instance.resume();
+}, 2100);
+
+
+setTimeout(function() {
+    console.log('testing stop/resume');
+    var totalDuration = instance.totalDuration();
+    console.log(instance.splits);
+    console.log('totalDuration: ' + totalDuration);
+    console.log(instance.splitDuration())
+}, 3100);
